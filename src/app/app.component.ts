6d34b66abe6b4ed4c66ef37d290397ea706e9e09
Angeles Segura <angye232000@gmail.com>
2023-04-27 11:56:45 -0600

    Angular Formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'prueba1';
+  registroForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.registroForm = this.fb.group({
+      nombreUsuario: ['', Validators.required],
+      correoElectronico: ['', [Validators.required, Validators.email]],
+      contrasena: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  submit() {
+    console.log('Registro enviado', this.registroForm.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms'; // Importar reactive forms module
+import { FormsModule } from '@angular/forms'; // Importar forms module
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, ReactiveFormsModule, FormsModule], // Importar los módulos
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
